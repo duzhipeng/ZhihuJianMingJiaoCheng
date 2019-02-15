03949a5265ffdb77b028b9e72e73d88d9c4eabf3
duzhipeng <dzp@intpi.com>
2019-02-15 11:22:44 +0800

    第十六期 ·  新利器之Pipenv

--- run.py
+++ run.py
@@ -2,10 +2,7 @@ from config import load_config
 from flask import Flask
 from flask import render_template
 
-
 app = Flask(__name__)
-# app.debug = True
-# app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 config = load_config()
 app.config.from_object(config)
 
